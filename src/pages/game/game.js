4fec5ed01a3b4f067c866fdf9fd369b5fb4c2838
Vincent Ramdhanie <vramdhanie@gmail.com>
2020-01-02 21:48:34 -0400

    Update Game page with grid layout and static positioned player boards

--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -1,15 +1,32 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 import Card from "../../components/card";
+import PlayerBoard from "../../components/playerBoard";
+import GameMenu from "../../components/gameMenu";
 
 const Game = ({ className }) => {
   return (
     <div className={className}>
-      Play the Game
-      <Link to="/">Home</Link>
-      <Card />
+      <div className="player1">
+        <PlayerBoard />
+      </div>
+      <div className="player2">
+        <PlayerBoard />
+      </div>
+      <div className="player3">
+        <PlayerBoard />
+      </div>
+      <div className="player4">
+        <PlayerBoard />
+      </div>
+      <div className="gameArea">
+        Play the Game
+        <Card />
+      </div>
+      <div className="gameLeft">
+        <GameMenu />
+      </div>
     </div>
   );
 };
@@ -17,9 +34,35 @@ const Game = ({ className }) => {
 export default styled(Game)`
   background: var(--secondaryLight);
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  display: grid;
   font-size: calc(10px + 2vmin);
+  grid-template:
+    "player1 player1 player2 player2"
+    "gameleft gamecenter gamecenter gameright"
+    "player3 player3 player4 player4";
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  column-gap: 4px;
+  row-gap: 4px;
+  padding: 4px;
+
+  .player1 {
+    grid-area: player1;
+  }
+
+  .player2 {
+    grid-area: player2;
+  }
+  .player3 {
+    grid-area: player3;
+  }
+  .player4 {
+    grid-area: player4;
+  }
+  .gameArea {
+    grid-area: gamecenter;
+  }
+
+  .gameLeft {
+    grid-area: gameleft;
+  }
 `;
